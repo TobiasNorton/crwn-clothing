8fb4d7483696a5e7324ffc30b1631d06830cdb7a
Tobias Norton <tnorton20@yahoo.com>
2021-06-16 21:36:11 -0400

    Remove unused code

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,45 +1,11 @@
 import React from 'react'
 import './style.scss'
 import Directory from '../../components/directory'
-import MenuItem from '../../components/menu-item'
 
 const HomePage = () => {
   return (
     <div className="homepage">
       <Directory />
-      {/* <div className="directory-menu">
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="category">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <MenuItem title="JACKETS" subtitle="SHOP NOW" />
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="category">JACKETS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="category">SNEAKERS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="category">MENS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="category">WOMENS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-      </div> */}
     </div>
   )
 }
