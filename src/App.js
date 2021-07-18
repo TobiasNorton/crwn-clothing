518236695ef5baf292d975ebc0b5ddeb65b876c6
Tobias Norton <tnorton20@yahoo.com>
2021-07-18 16:35:20 -0400

    Add and style Header component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import HomePage from './pages/homepage'
 import { Switch, Route } from 'react-router-dom'
 import ShopPage from './pages/shop-page'
+import Header from './components/header'
 
 const Hats = () => {
   return <h1>HATS!</h1>
@@ -11,6 +12,7 @@ const Hats = () => {
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
--- src/components/collection-preview/index.jsx
+++ src/components/collection-preview/index.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import './style.scss'
 import CollectionItem from '../collection-item'
 
-const CollectionPreview = ({ id, title, items }) => {
-  const firstFourItems = items.filter((item, index) => index < 4)
+const CollectionPreview = ({ title, items }) => {
+  const firstFourProducts = items.filter((item, index) => index < 4)
   return (
     <div className="collection-preview">
       <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
-        {firstFourItems.map((item) => {
+        {firstFourProducts.map((item) => {
           const { id, name, imageUrl, price } = item
           return <CollectionItem key={id} name={name} imageUrl={imageUrl} price={price} />
         })}
